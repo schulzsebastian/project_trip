4972ca05519635983b9b0b3a860d0600bbca8b82
schulzsebastian <schulz.siwy@gmail.com>
2016-06-27 23:11:45 +0200

    post plan in dashboard

--- app/api.py
+++ app/api.py
@@ -3,14 +3,20 @@
 
 from flask import abort, request, jsonify
 from flask_login import current_user
+from flask_cors import cross_origin
+from models import Plan
 from . import app
-from models import *
 
-@app.route('/plans/<nick>', methods=['GET'])
+@app.route('/plans/<nick>', methods=['GET', 'POST'])
+@cross_origin()
 def plans(nick):
     if request.method == 'GET':
         result = {'data':[]}
-        rs = Plan.select().where(Plan.nick==nick).dicts()
-        for row in rs:
+        p = Plan.select().where(Plan.nick==nick).dicts()
+        for row in p:
             result['data'].append(row)
         return jsonify(result)
+    if request.method == 'POST':
+    	payload = request.get_json(force=True)
+    	p = Plan.create(**payload)
+        return jsonify(payload)
